0e522aa0a07f65fe372bf1e540e98e8d81d57fe9
Oliver Richardson <oliver.w.k.richardson@gmail.com>
2023-12-12 09:57:30 +0000

    feat: added gameover and removed temp light

--- Assets/Scripts/Health Bar/HealthManager.cs
+++ Assets/Scripts/Health Bar/HealthManager.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HealthManager : MonoBehaviour
 {
     public float maxHealth;
     private float health;
     public HealthBar healthBar;
+    public GameObject gameoverScreen;
+    private float timer;
+    private bool timerActive;
 
     void Start()
     {
         health = maxHealth;
         healthBar.SetMaxHealth(maxHealth);
+        timerActive = false;
+        timer = 3.0f;
     }
 
     void Update()
     {
-        
+        if(timerActive)
+        {
+            timer -= Time.deltaTime;
+        }
+        if(timer <= 0.0f)
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 
     public void DamageBy(float amount)
@@ -24,6 +38,19 @@ public class HealthManager : MonoBehaviour
         health -= amount;
         Mathf.Clamp(health, 0, maxHealth);
         healthBar.SetHealth(health);
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        gameoverScreen.SetActive(true);
+        FirstPersonAIO FPAIO = GetComponent<FirstPersonAIO>();
+        FPAIO.playerCanMove = false;
+        FPAIO.enableCameraMovement = false;
+        timerActive = true;
     }
 
     public float GetHealth()
